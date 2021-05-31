db59e30d2bf87129fcbf5445b83859e63098eaff
DESKTOP-04IF1D6\ducan <ducanhkatori00@gmail.com>
2021-05-31 13:51:50 +0700

    feat: demo

---
+++ AppSeller.Models/Register.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AppSeller.Models
+{
+    public class Register
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType()]
+        public string Password { get; set; }
+        public bool Remember { get; set; }
+    }
+}
---
+++ AppSeller/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppSeller.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
